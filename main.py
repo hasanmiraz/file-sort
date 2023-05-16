61546fea503e7a32a6b9b2a0facb2c8b839e6839
Miraz <hasibul.miraz@gmail.com>
2023-05-17 01:56:25 +0600

    moving files

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
+import shutil
+
 if __name__=='__main__':
+
     # path of the target folder
     path = ".\\test"
+    dest = ".\\result\\"
 
     # check if the path exists
     if os.path.exists(path):
         count_dirs = 0
         count_files = 0
         list_of_folders = []
+        dict_of_files = {}
         set_of_extensions = set()
         
         # get all the file names in the directory
@@ -24,17 +29,28 @@ if __name__=='__main__':
             else:
                 count_files+=1
                 _, file_extension = os.path.splitext(path+"\\"+file)
-                set_of_extensions.add(file_extension.replace(".",""))
+                file_extension = file_extension.replace(".","")
+                
+                if file_extension not in set_of_extensions:
+                    set_of_extensions.add(file_extension)
+                    dict_of_files.update({file_extension:[]})
+                
+                dict_of_files[file_extension].append(file)
+                
 
         # creating directories
         for extention in set_of_extensions:
-            if not os.path.exists(path+"\\result\\"+str(extention)):
-                os.makedirs(path+"\\result\\"+str(extention))
+            if not os.path.exists(dest+str(extention)):
+                os.makedirs(dest+str(extention))
 
+        # moving files
+        for key, values in dict_of_files.items():
+            for value in values:
+                shutil.move(path+"\\"+value, dest+key+"\\"+value)
 
 
         print(f"dir  = {count_dirs}")
-        print(f"set  = {count_files}")
+        print(f"files  = {count_files}")
 
 
     else:
