f42da83073f6f8165d41a3074a6f44936f0978c1
Miraz <hasibul.miraz@gmail.com>
2023-05-17 02:08:23 +0600

    main done

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import shutil
 if __name__=='__main__':
 
     # path of the target folder
-    path = ".\\test"
-    dest = ".\\result\\"
+    path = input("enter the input path: ")+"\\"
+    dest = input("enter the output path: ")+"\\"
 
     # check if the path exists
     if os.path.exists(path):
@@ -21,14 +21,14 @@ if __name__=='__main__':
         # print(files)
         for file in files:
             # for directories
-            if os.path.isdir(path+"\\"+file):
+            if os.path.isdir(path+file):
                 count_dirs+=1
                 list_of_folders.append(file)
             
             # for files
             else:
                 count_files+=1
-                _, file_extension = os.path.splitext(path+"\\"+file)
+                _, file_extension = os.path.splitext(path+file)
                 file_extension = file_extension.replace(".","")
                 
                 if file_extension not in set_of_extensions:
@@ -46,7 +46,7 @@ if __name__=='__main__':
         # moving files
         for key, values in dict_of_files.items():
             for value in values:
-                shutil.move(path+"\\"+value, dest+key+"\\"+value)
+                shutil.move(path+value, dest+key+"\\"+value)
 
 
         print(f"dir  = {count_dirs}")
